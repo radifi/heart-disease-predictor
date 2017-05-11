e0ad9ceac2ba6e42591c71715de4e64b31589322
csachar <cjiced@gmail.com>
2017-05-11 16:42:08 -0400

    Update flaskr.py

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,8 +1,13 @@
-
-#!/usr/bin/env python
+#!/usr/bin/python3.6
 # all the imports
+from __future__ import print_function
 import os
-import sqlite3
+import subprocess
+from io import StringIO
+from subprocess import call
+import PIL
+from PIL import Image
+#import sqlite3
 #import sys
 #import shutil
 #import time
@@ -10,8 +15,8 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 #from django.conf.urls import include
-     
-import statsmodels.api as sm
+
+#import statsmodels.api as sm
 from sklearn.cross_validation import KFold
 from sklearn.metrics import confusion_matrix
 from sklearn import linear_model
@@ -21,7 +26,7 @@ from sklearn.neighbors import KNeighborsClassifier as KNN
 from sklearn.metrics import roc_curve, auc
 from sklearn.utils import shuffle
 from sklearn import svm
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.externals import joblib
@@ -36,7 +41,7 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 import pandas as pd
 import pylab as plt
-import seaborn
+#import seaborn
 import numpy.random as nprnd
 import random
 import json
@@ -45,173 +50,108 @@ import pickle
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.tree import DecisionTreeRegressor
 from IPython.display import Image
-import pydotplus 
+import pydotplus
 import pydot
 import graphviz
-     
+from sklearn import tree
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#import importlib.util
+#spec = importlib.util.spec_from_file_location("graphviz", "/home/cs3514/.local/lib/python3.5/site-packages/graphviz/files.py")
+#foo = importlib.util.module_from_spec(spec)
+#spec.loader.exec_module(foo)
+#foo.MyClass()
+
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
-training_data = 'data/HeartData_ALL.csv'
-#dependent_variable = include[-1]
+my_dir = os.path.dirname(__file__)
+pickle_file_path = os.path.join(my_dir,'CLI_pickle_file.pkl')
+with open(pickle_file_path,'rb') as pickle_file:
+    decision_tree_CLI = pickle.load(pickle_file)
 
-# Loading the saved decision tree model pickle
-decision_tree_pkl_filename = 'heart_disease_decision_regressor_tree.pkl'
-decision_tree_model_pkl = open(decision_tree_pkl_filename, 'rb')
-decision_tree_model = pickle.load(decision_tree_model_pkl)
-#print ("Loaded Decision tree model :: ", decision_tree_model)
+my_dir_fram = os.path.dirname(__file__)
+pickle_file_fram_path = os.path.join(my_dir_fram,'framingham_pickle_file.pkl')
+with open(pickle_file_fram_path,'rb') as pickle_file_fram:
+    decision_tree_fram = pickle.load(pickle_file_fram)
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DATABASE2=os.path.join(app.root_path, 'patient.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+def visualize_tree(tree, feature_names):
+    """Create tree png using graphviz.
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-    
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-    
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
+    Args
+    ----
+    tree -- scikit-learn DecsisionTree.
+    feature_names -- list of feature names.
     """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-    
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-        
-def connect_db2():
-    """Connects to the specific database."""
-    rv2 = sqlite3.connect(app.config['DATABASE2'])
-    rv2.row_factory = sqlite3.Row
-    return rv2
-    
-def init_db2():
-    db2 = get_db2()
-    with app.open_resource('schema2.sql', mode='r') as f2:
-        db2.cursor().executescript(f2.read())
-    db2.commit()
+    with open("dt.dot", 'w') as f:
+        export_graphviz(tree, out_file=f)
 
-@app.cli.command('initdb2')
-def initdb2_command():
-    """Initializes the database2."""
-    init_db2()
-    print('Initialized the database2.')
-    
-def get_db2():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db2'):
-        g.sqlite_db2 = connect_db2()
-    return g.sqlite_db2
-    
-@app.teardown_appcontext
-def close_db2(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db2'):
-        g.sqlite_db2.close()
-        
+    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
+    try:
+        subprocess.check_call(command)
+    except:
+        exit("Could not run dot, ie graphviz, to "
+             "produce visualization")
+
+visualize_tree(decision_tree_fram, feature_names=['x'])
+
+features = ['Sex', 'Tot Chol', 'Age', 'Systolic', 'Diastolic', 'Smoker?',
+                 'Num Cig','BMI', 'Diabetes', 'BP meds?', 'Heart rate', 'Glucose','Education']
+target = ['Low risk', 'High risk']
+dot_data = StringIO()
+dot_data = export_graphviz(decision_tree_fram, out_file=None,
+                        feature_names=features,
+                         class_names=target,
+                         filled=True, rounded=True,
+                         special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data)
+Image(graph.create_png())
+
 @app.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('select age, gender, Cp, Trestbps, Chol, Fbs, Restecg, Thalach, Exang, Old_Peak_ST, Slope, Ca, Thal from entries order by id desc')
-    entries = cur.fetchall()
-    db2 = get_db2()
-    #raise Exception(db2.execute('select * from patient'))
-    cur2 = db2.execute('select SEX, TOTCHOL, AGE, SYSBP, DIABP, CURSMOKE, CIGPDAY, BMI, DIABETES, EDUC, HEARTRTE, GLUCOSE, BPMEDS from patient order by id2 desc')
-    patient = cur2.fetchall()
-    return render_template('show_entries.html', entries = entries, patient=patient)
-    
+    return render_template('show_entries.html')
+
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if request.form['age'] == "" or request.form['gender'] == "":
+    if request.form['age'] == "" or request.form['gender'] == "" or request.form['Cp'] == "" or \
+    request.form['Trestbps'] == "" or request.form['Chol'] =="" or request.form['Fbs'] == "" or \
+    request.form['Restecg'] == "" or request.form['Thalach'] == "" or request.form['Exang'] == ""\
+    or request.form['Old_Peak_ST'] == "" or request.form['Slope'] == "" or request.form['Ca'] == ""\
+    or request.form['Thal'] == "":
         return render_template("failure.html")
-    #if not session.get('logged_in'):
-        #abort(401)
-    db = get_db()
-    db.execute('insert into entries (age, gender, Cp, Trestbps, Chol, Fbs, Restecg, Thalach, Exang, Old_Peak_ST, Slope, Ca, Thal) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                 [request.form['age'], request.form['gender'], request.form['Cp'], request.form['Trestbps'], request.form['Chol'], request.form['Fbs'], request.form['Restecg'], request.form['Thalach'], request.form['Exang'], request.form['Old_Peak_ST'], request.form['Slope'], request.form['Ca'], request.form['Thal']])
-    user_data = db.commit()
     query = [request.form['age'], request.form['gender'], request.form['Cp'], request.form['Trestbps'], request.form['Chol'], request.form['Fbs'], request.form['Restecg'], request.form['Thalach'], request.form['Exang'], request.form['Old_Peak_ST'], request.form['Slope'], request.form['Ca'], request.form['Thal']]
-    prediction = decision_tree_model.predict(query)
-    query_test = ''.join(str(e) for e in query)
+    prediction = decision_tree_CLI.predict(query)
+    predict_CLI_prob = decision_tree_CLI.predict_proba(query)
+    path_CLI = decision_tree_CLI.decision_path(query)
+
+    prob_score_string_CLI = "{:.3f}".format(np.amax(predict_CLI_prob)*100)
     predict2 = np.array_str(prediction)
-    return '{}'.format(predict2)
-    #return '{} {}'.format(query_test, predict2)
-    #db.commit()
-    #flash('New entry was successfully posted')
-    #return redirect(url_for('show_entries'))
+    if prediction == 1:
+        return render_template('image_display2.html', outcome_clev='You are at risk for heart disease. Probability score of: ' + prob_score_string_CLI)
+    else:
+        return render_template('image_display2.html', outcome_clev='Congrats! You are not at risk for heart disease. Probability score of: ' + prob_score_string_CLI)
+
 
 @app.route('/add2', methods=['POST'])
 def add_entry2():
-    if request.form['SEX'] == "":
+    if request.form['SEX'] == "" or request.form['TOTCHOL'] == "" or \
+    request.form['AGE'] == "" or request.form['SYSBP'] == "" or \
+    request.form['DIABP'] == "" or request.form['CURSMOKE'] == "" or \
+    request.form['CIGPDAY'] == "" or request.form['BMI'] == "" or \
+    request.form['DIABETES'] == "" or request.form['BPMEDS'] == "" or\
+    request.form['HEARTRTE'] == "" or request.form['GLUCOSE'] == "" or\
+    request.form['EDUC']:
         return render_template("failure.html")
-    #if not session.get('logged_in'):
-        #abort(401)
-    db2 = get_db2()
-    db2.execute('insert into patient (SEX, TOTCHOL, AGE, SYSBP, DIABP, CURSMOKE, CIGPDAY, BMI, DIABETES, EDUC, HEARTRTE, GLUCOSE, BPMEDS) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                 [request.form['SEX'], request.form['TOTCHOL'], request.form['AGE'], request.form['SYSBP'], request.form['DIABP'], request.form['CURSMOKE'], request.form['CIGPDAY'], request.form['BMI'], request.form['DIABETES'], request.form['EDUC'], request.form['HEARTRTE'], request.form['GLUCOSE'], request.form['BPMEDS']])
-    user_data2 = db2.commit()
-    #print ("hello2")
-    query2 = [request.form['SEX'], request.form['TOTCHOL'], request.form['AGE'], request.form['SYSBP'], request.form['DIABP'], request.form['CURSMOKE'], request.form['CIGPDAY'], request.form['BMI'], request.form['DIABETES'], request.form['EDUC'], request.form['HEARTRTE'], request.form['GLUCOSE'], request.form['BPMEDS']]
-    query_test2 = ''.join(str(e) for e in query2)
-    return '{}'.format(query_test2)
-    #print ("hello")
-    #db2.execute('insert into patient (SEX, TOTCHOL, AGE, SYSBP, DIABP, CURSMOKE, CIGPDAY, BMI, DIABETES, BPMEDS, HEARTRTE, GLUCOSE, educ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    #             [request.form['SEX'], request.form['TOTCHOL'], request.form['AGE'], request.form['SYSBP'], request.form['DIABP'], request.form['CURBSMOKE'], request.form['CIGPDAY'], request.form['BMI'], request.form['DIABETES'], request.form['BPMEDS'], request.form['HEARTRTE'], request.form['GLUCOSE'], request.form['educ']])
-    #user_data2 = db2.commit()
-    #print ("hello2")
-    #query2 = [request.form['SEX'], request.form['TOTCHOL'], request.form['AGE'], request.form['SYSBP'], request.form['DIABP'], request.form['CURBSMOKE'], request.form['CIGPDAY'], request.form['BMI'], request.form['DIABETES'], request.form['BPMEDS'], request.form['HEARTRTE'], request.form['GLUCOSE'], request.form['educ']]
-    #query_test2 = ''.join(str(e) for e in query2)
-    #return '{}'.format(query_test2)
-    
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-    
-#@app.route('/predict', methods=['POST'])
-#def predict():	
-#	query = [65,1,3.4,249,230,1,2,240,1,2.45,2,2.45,3]
-#	prediction = decision_tree_model.predict(query)
-#	predict2 = np.array_str(prediction)
-#	return predict2
+    query2 = [request.form['SEX'], request.form['TOTCHOL'], request.form['AGE'], request.form['SYSBP'], request.form['DIABP'], request.form['CURSMOKE'], request.form['CIGPDAY'], request.form['BMI'], request.form['DIABETES'], request.form['BPMEDS'], request.form['HEARTRTE'], request.form['GLUCOSE'], request.form['EDUC']]
+    predict_fram = decision_tree_fram.predict(query2)
+    predict_fram_prob = decision_tree_fram.predict_proba(query2)
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    path_fram = decision_tree_fram.decision_path(query)
+    prob_score_string_fram = "{:.3f}".format(np.amax(predict_fram_prob)*100)
+    query_test2 = np.array_str(predict_fram)
+
+    if predict_fram == 1:
+        return render_template('image_display.html', outcome_fram='You are at risk for heart disease. Probability score of: ' + prob_score_string_fram)
+    else:
+        return render_template('image_display.html', outcome_fram='Congrats! You are not at risk for heart disease. Probability score of: ' + prob_score_string_fram)
